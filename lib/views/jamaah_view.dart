1e88762c76a4c16eebc3e19de0f72f34475c5e3d
bayuence <bayuence@gmail.com>
2026-01-08 22:35:33 +0700

    6 jamaah view

---
+++ lib/views/jamaah_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class JamaahView extends StatelessWidget {
+  const JamaahView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daftar Jamaah'),
+      ),
+      body: const Center(
+        child: Text('View siap'),
+      ),
+    );
+  }
+}
