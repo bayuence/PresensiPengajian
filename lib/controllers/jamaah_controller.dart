5a6f812242da4947241919df2bb8a606154bc636
bayuence <bayuence@gmail.com>
2026-01-09 00:15:32 +0700

    8 jamaah

--- lib/controllers/jamaah_controller.dart
+++ lib/controllers/jamaah_controller.dart
@@ -1,18 +1,35 @@
-
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/jamaah.dart';
 
 class JamaahController {
-  static Future<String> fetchJamaah() async {
-    final url = Uri.parse(
-      'http://localhost/presensi_pengajian/jamaah.php',
-    );
+  static const String _baseUrl =
+      'http://localhost/presensi_pengajian/jamaah.php';
 
-    final response = await http.get(url);
+  /// Ambil data JSON mentah dari API
+  static Future<String> fetchJamaahRaw() async {
+    final response = await http.get(Uri.parse(_baseUrl));
 
     if (response.statusCode == 200) {
-      return response.body; // JSON masih String
+      return response.body;
     } else {
       throw Exception('Gagal mengambil data jamaah');
     }
+  }
+
+  /// Ambil data jamaah dalam bentuk List<Jamaah>
+  static Future<List<Jamaah>> fetchJamaahModel() async {
+    final String rawData = await fetchJamaahRaw();
+    final Map<String, dynamic> jsonData = jsonDecode(rawData);
+
+    List<Jamaah> jamaahList = [];
+
+    if (jsonData['result'] == 'success') {
+      for (var item in jsonData['data']) {
+        jamaahList.add(Jamaah.fromJson(item));
+      }
+    }
+
+    return jamaahList;
   }
 }
--- lib/views/jamaah_view.dart
+++ lib/views/jamaah_view.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
 import '../controllers/jamaah_controller.dart';
+import '../models/jamaah.dart';
 
-class JamaahView extends StatelessWidget {
+class JamaahView extends StatefulWidget {
   const JamaahView({super.key});
+
+  @override
+  State<JamaahView> createState() => _JamaahViewState();
+}
+
+class _JamaahViewState extends State<JamaahView> {
+  late Future<List<Jamaah>> futureJamaah;
+
+  @override
+  void initState() {
+    super.initState();
+    futureJamaah = JamaahController.fetchJamaahModel();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Daftar Jamaah')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            final data = await JamaahController.fetchJamaah();
-            print(data); // lihat di Debug Console
-          },
-          child: const Text('Ambil Data Jamaah'),
-        ),
+      appBar: AppBar(
+        title: const Text('Data Jamaah'),
+      ),
+      body: FutureBuilder<List<Jamaah>>(
+        future: futureJamaah,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          final jamaahList = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: jamaahList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.person),
+                title: Text(jamaahList[index].nama),
+                subtitle: Text(
+                  'Status aktif: ${jamaahList[index].statusAktif}',
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
